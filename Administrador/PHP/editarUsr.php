a893c041e369c381741ee86560337802a1c8ef8f
ProyectoSUGAR <proyectoithink5@gmail.com>
2025-10-06 17:42:53 -0300

    Update editarUsr.php

--- Administrador/PHP/editarUsr.php
+++ Administrador/PHP/editarUsr.php
@@ -10,11 +10,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['id_usuario'])) {
     $tipo_usuario = $_POST['tipo_usuario'];
     mysqli_query($conexion, "UPDATE usuario SET nombre='$nombre', apellido='$apellido', correo='$correo', tipo_usuario='$tipo_usuario' WHERE id_usuario=$id_usuario");
 
-    echo "<script>window.location.href='../HTML/gestionUsr.php';</script>";
+    echo "<script>window.location.href='../../HTML/gestionUsr.php';</script>";
     exit;
 } else {
 
-    header("Location: ../HTML/gestionUsr.php");
+    header("Location: ../../HTML/gestionUsr.php");
     exit;
 }
 ?>
