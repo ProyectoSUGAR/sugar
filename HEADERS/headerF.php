549e0ad883adfe35ba0494c3b64528f0fdb5d18c
ProyectoSUGAR <proyectoithink5@gmail.com>
2025-10-16 15:03:39 -0300

    Update headerF.php

--- HEADERS/headerF.php
+++ HEADERS/headerF.php
@@ -46,8 +46,6 @@
     <!-- Inclusión del script que gestiona la funcionalidad del menú hamburguesa -->
     <script src="/JS/menuHamb.js"></script>
 </header>
-<<<<<<< HEAD
-=======
 
 <script>
 fetch('/PHP/notificaciones_usuario.php?tipo_usuario=funcionario')
@@ -62,4 +60,3 @@ fetch('/PHP/notificaciones_usuario.php?tipo_usuario=funcionario')
     })
     .catch(error => console.error('Error al cargar notificaciones:', error));
 </script>
->>>>>>> dff50c8 (Act)
