076d2b5dc98e8717c79107b4283cb818b40917c1
ProyectoSUGAR <proyectoithink5@gmail.com>
2025-10-16 14:54:36 -0300

    Add 'Edit profile' link to header

--- HEADERS/headerP.php
+++ HEADERS/headerP.php
@@ -14,6 +14,8 @@
         <div class="datos-usuario">
             <strong >Profesor/a</strong>
             <br>
+            <a  class="p1" href="/../../Profesor/HTML/editarPerfil.php">Editar perfil</a>
+            <br>
             <a  class="p1" href="../../Login/HTML/ingreso.php">Cerrar sesión</a>
             
         </div>
